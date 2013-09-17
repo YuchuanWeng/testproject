52460f4a7ac3318eba88d3d94d16b04cec496736
YuchuanWeng <cts734@gmail.com>
2013-09-17 17:22:46 +0200

    methods from chapter 6

---
+++ moreaboutmethod.rb
@@ -0,0 +1,16 @@
+
+
+puts (5-2).abs
+puts (2-5).abs
+
+puts rand
+puts rand
+puts rand
+puts (rand(100))
+puts (rand(1))
+puts (rand(10))
+
+puts ('The weatherman said there is a')
+puts (rand(101).to_s + '% chance of rain,')
+puts ('but you can never trust a weatherman.')
+
