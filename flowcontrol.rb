e4d426a1a61bac7d7861931d5f6d9aa27c4fe012
YuchuanWeng <cts734@gmail.com>
2013-09-18 08:46:02 +0200

    chapter7section1

---
+++ flowcontrol.rb
@@ -0,0 +1,38 @@
+
+puts 1 > 2
+puts 1 < 2
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts 1 == 1
+puts 2 != 1
+
+puts 'cat' < 'dog'
+
+puts 'bug lady'                < 'Xander'
+puts 'bug lady'.downcase       < 'Xander'.downcase
+
+puts 2 < 10
+puts '2' < '10'
+
+#Branching
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Weng'
+  puts 'What a lovely name!'
+end
+
+puts 'I am a fortune-teller. Tell me your name:'
+name = gets.chomp
+
+if name = 'Chris'
+  puts 'I see great things in your future.'
+else
+  puts 'Your future is... oh my! Look at the time!'
+  puts 'I really have to go, sorry!'
+end
+  
