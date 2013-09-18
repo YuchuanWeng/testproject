8e9484954c3f89e817a011662a6a94760f8004ef
YuchuanWeng <cts734@gmail.com>
2013-09-18 09:12:36 +0200

    chapter7rest

--- flowcontrol.rb
+++ flowcontrol.rb
@@ -29,10 +29,45 @@ end
 puts 'I am a fortune-teller. Tell me your name:'
 name = gets.chomp
 
-if name = 'Chris'
+if name == 'Chris'
   puts 'I see great things in your future.'
 else
   puts 'Your future is... oh my! Look at the time!'
   puts 'I really have to go, sorry!'
 end
-  
+
+#Looping
+
+input = ''
+while input != 'bye'
+  puts input
+  input = gets.chomp
+end
+puts 'Come again soon!'
+
+while true
+  input = gets.chomp
+  puts input
+  if input == 'bye'
+    break
+  end
+end
+
+puts 'Come again soon!'
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris' || name == 'katy'
+  puts 'What a lovely name!'
+end
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+few_name == ['Chris', 'Katy', 'Weng']
+if name == few_name[2]
+	puts 'What a lovely name!'
+end
