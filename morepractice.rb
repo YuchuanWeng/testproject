1ef09fbec37959eccd19a73d3ca09958853d0a5a
YuchuanWeng <cts734@gmail.com>
2013-09-26 13:15:35 +0200

    firstmorepractice

---
+++ morepractice.rb
@@ -0,0 +1,11 @@
+#more practice
+
+puts "What's your favorite city?"
+city_name = gets.chomp
+puts "What's your favorite car?"
+car_name = gets.chomp
+puts "what is your favorite time to drive your dream car?"
+fav_time = gets.chomp
+
+puts "It's awesome to drive #{car_name} at #{fav_time} in #{city_name}."
+
